e3777fe7ddccc6c4f1a801980ed503f592d61710
redd <redd0394832@gmail.com>
2024-11-01 03:00:34 +0200

    rework lool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
-import { FontawesomeObject, IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+    FontawesomeObject,
+    IconProp,
+} from "@fortawesome/fontawesome-svg-core";
+import {
+    faDiscord,
+    faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Url } from "next/dist/shared/lib/router/router";
-
-function Card({ children }: React.PropsWithChildren<{}>) {
-    return (
-        <div className="border-zinc-700 bg-gradient-to-b from-zinc-800 to-zinc-900 shadow-2xl border p-4 w-64 m-3 items-center text-center min-h-52">
-            {children}
-        </div>
-    );
-}
-
-function LargeCard({ children }: React.PropsWithChildren<{}>) {
-    return (
-        <div className="border-zinc-700 bg-gradient-to-b from-zinc-800 to-zinc-900 shadow-2xl border p-4 w-96 max-w-full m-3 items-center text-center min-h-52">
-            {children}
-        </div>
-    );
-}
 
 function SocialIcon({
     icon,
@@ -29,102 +19,73 @@ function SocialIcon({
     link?: string;
 }) {
     return (
-        <a className="flex m-1 mr-4" href={link}>
+        <a className="flex m-1 mr-4 text-gray-400" href={link}>
             <div className="aspect-square w-7">
-                <FontAwesomeIcon className={"w-6 aspect-square"} icon={icon}/>
+                <FontAwesomeIcon
+                    className={"w-6 aspect-square"}
+                    icon={icon}
+                />
             </div>
             {text}
         </a>
     );
 }
 
+function Card({
+    name,
+    description,
+}: {
+    name: string;
+    description: string;
+}) {
+    return (
+        <div className="min-w-[12rem] min-h-[10rem] text-center max-w-full p-1 m-1 bg-zinc-900 border border-zinc-700 rounded-sm">
+            <p className="text-gray-300">{name}</p>
+            <hr className="border-zinc-400"/>
+            <p className="text-zinc-400">{description}</p>
+        </div>
+    );
+}
+
 export default function Site() {
     return (
         <div className=" text-gray-300 justify-center items-center">
-            <div className="bg-gradient-to-b from-zinc-800 to-zinc-900 h-screen flex justify-center items-center">
-                <div className="w-[32rem] max-w-full text-center shadow-2xl bg-zinc-900  border border-zinc-700">
-                    <div className=" mt-3" />
-                    <p className="mb-3">hello! i am redd</p>
-                    <p className="mb-3">i am a programmer</p>
-                    <p className="mb-3">
-                        i usually develop in roblox, but sometimes make things
-                        outside of it too
+            <div className="bg-gradient-to-b from-zinc-900 to-zinc-950 h-screen flex justify-center items-center">
+                <div className="w-[48rem] p-3 max-w-full text-center bg-[#141418]  border border-zinc-800 rounded-md">
+                    <p className="">
+                        hello! i am redd
                     </p>
-                    <p className="mb-4">
-                        i code mostly in lua and typescript, but sometimes
-                        python too
-                    </p>
-                    <p className="mb-4">
-                        mostly interested in tactical shooters
-                    </p>
-                    <div className="justify-center flex">
-                        <SocialIcon icon={faDiscord} text="dder0" />
-                        <SocialIcon icon={faGithub} text="redd-ok" link="https://github.com/redd-ok"/>
+
+                    <div className="justify-center flex mb-3">
+                        <SocialIcon
+                            icon={faDiscord}
+                            text="dder0"
+                        />
+                        <SocialIcon
+                            icon={faGithub}
+                            text="redd-ok"
+                            link="https://github.com/redd-ok"
+                        />
+                    </div>
+
+                    <div className="w-[40rem] max-w-full place-self-center p-1 bg-zinc-900 border border-zinc-800 rounded-sm">
+                        <p className="mb-1">
+                            Projects
+                        </p>
+                        <div className="w-[28rem] max-w-full place-self-center p-1 border-zinc-700 rounded-sm grid-cols-4 flex justify-center">
+                                <Card
+                                    name="RCS"
+                                    description="Custom weapon system"
+                                /><Card
+                                    name="thinh 1"
+                                    description="Phasellus auctor eleifend efficitur. Vivamus vehicula lectus in augue feugiat, eget varius dolor molestie. Vestibulum."
+                                /><Card
+                                    name="thing 2"
+                                    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris feugiat felis in arcu lacinia, nec."
+                                />
+                        </div>
                     </div>
                 </div>
-            </div>
-
-            <p className="relative bottom-12 text-center mb-2">
-                Experienced with
-            </p>
-            <hr className="relative bottom-12" />
-
-            <div className="h-full grid md:flex justify-center items-center mb-12">
-                <Card>
-                    <p className="mb-2">Roblox development tools</p>
-                    <hr className="mb-2" />
-                    im decently experienced at rojo and wally, having worked on
-                    a fully rojo managed project and partially aswell
-                </Card>
-                <Card>
-                    <p className="mb-2">Lua</p>
-                    <hr className="mb-2" />
-                    ive been coding lua for 4+ years now, most of my coding
-                    experience comes from lua and in the past use to work on a
-                    custom compiler and interpreter for lua 5.1, i mostly code
-                    lua for roblox however
-                </Card>
-                <Card>
-                    <p className="mb-2">Typescript & Javascript</p>
-                    <hr className="mb-2" />i have some experience with
-                    typescript and javascript, ive made websites and discord
-                    bots before using typescript, i am still more experienced in
-                    lua however, i have been recently working with roblox-ts alongside flamework, and react-lua inside personal projects
-                </Card>
-                <Card>
-                    <p className="mb-2">Roblox</p>
-                    <hr className="mb-2" />
-                    roblox is what mainly inspired me to learn programming and
-                    game development as it was alot more simple back then, i
-                    have experience coding in front-end and back-end for roblox
-                    games, and have a decent knowledge about game security
-                </Card>
-                <Card>
-                    <p className="mb-2">Gun Engines for Roblox</p>
-                    <hr className="mb-2" />i have alot of experience in creating
-                    gun engines, having been commisioned once before for a 20+
-                    million visits game, i mostly like creating more tactical
-                    gun engines but sometimes ill have todo arcady gun engines
-                </Card>
-                <Card>
-                    <p className="mb-2">Anti-Cheats</p>
-                    <hr className="mb-2" />i am some-what decent at developing and bypassing anti-cheats, i used to exploit on roblox which let me gain alot of knowledge about security in roblox games and general security across all games, i do not exploit on roblox games anymore and i do not plan todo so without explicit permission
-                </Card>
-            </div>
-
-            <p className="relative text-center mb-2">Projects</p>
-            <hr className="relative mb-20" />
-
-            <div className="h-full grid md:flex justify-center items-center mb-12">
-                <LargeCard>
-                    <p className="mb-2">Airsoft Center 2</p>
-                    <hr className="mb-2" />i am a developer for airsoft center,
-                    a game which has 20+ million visits, i joined the
-                    development team shortly after i was commisioned to make a
-                    gun engine for them, i was tasked with back-end coding
-                    aswell as front-end, i scripted the gamemodes for the game
-                    aswell as the entire gun engine
-                </LargeCard>
             </div>
         </div>
     );
