624d77d79df562d4e101683e3448f43de2af3538
redd <redd0394832@gmail.com>
2024-11-08 16:55:54 +0200

    replace roblox icon with a svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,11 +40,11 @@ function SocialIcon({
 }
 
 function CustomIcon({
-    iconURL,
+    svg,
     text,
     link,
 }: {
-    iconURL: string;
+    svg: JSX.Element;
     text: string;
     link?: string;
 }) {
@@ -58,13 +58,13 @@ function CustomIcon({
             }
             href={link}
         >
-            <Image
-                src={iconURL}
+            {svg}
+            {/* <Image
                 alt=""
                 className="aspect-square w-6 h-6 mr-1"
                 width={0}
                 height={0}
-            />
+            /> */}
             {text}
         </a>
     );
@@ -136,7 +136,19 @@ export default function Site() {
                         link="https://github.com/redd-ok"
                     />
                     <CustomIcon
-                        iconURL="https://images.rbxcdn.com/7bba321f4d8328683d6e59487ce514eb"
+                        svg={
+                            <div className="aspect-square w-7">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    x="0px"
+                                    y="0px"
+                                    className="w-6 aspect-square fill-slate-300"
+                                    viewBox="0 0 50 50"
+                                >
+                                    <path d="M 12.125 1.9980469 A 1.0001 1.0001 0 0 0 11.199219 2.7441406 L 2.0332031 37.576172 A 1.0001 1.0001 0 0 0 2.7460938 38.798828 L 37.580078 47.966797 A 1.0001 1.0001 0 0 0 38.802734 47.253906 L 47.96875 12.419922 A 1.0001 1.0001 0 0 0 47.255859 11.197266 L 12.421875 2.03125 A 1.0001 1.0001 0 0 0 12.125 1.9980469 z M 21.5 19 L 31 21.5 L 28.5 31 L 19 28.5 L 21.5 19 z"></path>
+                                </svg>
+                            </div>
+                        }
                         text="0_redd"
                         link="https://www.roblox.com/users/1482715772/profile"
                     />
